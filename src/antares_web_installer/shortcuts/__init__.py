ca219503f3b192c844ab78394382a5b19ed24164
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2024-06-11 16:05:08 +0200

    docs: add docstring to the `shortcuts` module

--- src/antares_web_installer/shortcuts/__init__.py
+++ src/antares_web_installer/shortcuts/__init__.py
@@ -10,14 +10,23 @@ _shell_module = importlib.import_module(f"_{sys.platform}_shell", _PKG)
 
 
 def get_homedir() -> str:
+    """
+    Return the path to the user's home directory.
+    """
     return _shell_module.get_homedir()
 
 
 def get_desktop() -> str:
+    """
+    Return the path to the user's desktop directory.
+    """
     return _shell_module.get_desktop()
 
 
 def get_start_menu() -> str:
+    """
+    Return the path to the user's start menu directory.
+    """
     return _shell_module.get_start_menu()
 
 
@@ -30,6 +39,17 @@ def create_shortcut(
     icon_path: t.Union[str, os.PathLike] = "",
     description: str = "",
 ) -> None:
+    """
+    Creates a shortcut to an executable.
+
+    Args:
+        target: The path to the shortcut file to be created.
+        exe_path: The path to the executable that the shortcut should launch.
+        arguments: The arguments to be passed to the executable.
+        working_dir: The working directory for the executable.
+        icon_path: The path to the icon for the shortcut.
+        description: The description of the shortcut.
+    """
     return _shell_module.create_shortcut(
         target,
         exe_path,
