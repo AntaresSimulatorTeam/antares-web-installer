c8c09c39911b7e7a4a820c3f067e21750b0ed0cb
Maurane GLAUDE <mauranne.glaude_externe@rte-france.com>
2024-06-24 17:14:18 +0200

    fix(config): conditional upgrade of config file

--- src/antares_web_installer/config/config_2_15.py
+++ src/antares_web_installer/config/config_2_15.py
@@ -18,17 +18,19 @@ def update_to_2_15(config: t.MutableMapping[str, t.Any]) -> None:
     # end of WIP
 
     config.setdefault("launcher", {})
-    config["launcher"].setdefault("local", {})
-    config["launcher"].setdefault("slurm", {})
-    config["launcher"]["local"]["enable_nb_cores_detection"] = True
 
-    default_n_cpu = config["launcher"]["slurm"].pop("default_n_cpu", None)
-    if default_n_cpu is not None:
-        nb_cores_default = max(nb_cores_min, min(default_n_cpu, nb_cores_max))
+    if "local" in config["launcher"]:
+        config["launcher"]["local"]["enable_nb_cores_detection"] = True
 
-    config["launcher"]["slurm"]["enable_nb_cores_detection"] = False
-    config["launcher"]["slurm"]["nb_cores"] = {
-        "min": nb_cores_min,
-        "default": nb_cores_default,
-        "max": nb_cores_max,
-    }
+    if "slurm" in config["launcher"]:
+        slurm_config = config["launcher"]["slurm"]
+        default_n_cpu = slurm_config.pop("default_n_cpu", None)
+        if default_n_cpu is not None:
+            nb_cores_default = max(nb_cores_min, min(default_n_cpu, nb_cores_max))
+
+        slurm_config["enable_nb_cores_detection"] = False
+        slurm_config["nb_cores"] = {
+            "min": nb_cores_min,
+            "default": nb_cores_default,
+            "max": nb_cores_max,
+        }
