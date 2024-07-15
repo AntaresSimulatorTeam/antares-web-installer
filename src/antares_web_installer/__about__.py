fba3cb1a813c8fb68a4997b3a0ef871781ad0221
Maurane GLAUDE <mauranne.glaude_externe@rte-france.com>
2024-07-15 15:03:07 +0200

    fix(workflow): fix invalid syntax in `python-package.yml`

--- src/antares_web_installer/__about__.py
+++ src/antares_web_installer/__about__.py
@@ -4,7 +4,7 @@ Antares Web Installer
 This module contains the project metadata.
 """
 
-__version__ = "0.2.0-alpha"
+__version__ = "0.2.0"
 __author__ = "RTE, Antares Web Team"
 __date__ = "2024-04-23"
 __credits__ = "(c) Réseau de Transport de l’Électricité (RTE)"
