a0d7a1192a2912daa874001394247f7deff96113
Anis SMAIL <anis.smail_externe@rte-france.com>
2025-04-15 09:59:26 +0200

    remove workspaces from config other than default workspace

--- src/antares_web_installer/config/config_desktop.py
+++ src/antares_web_installer/config/config_desktop.py
@@ -1,10 +1,28 @@
 import typing as t
 
+from antares_web_installer import logger
+
+
 
 def update_for_desktop(config: t.MutableMapping[str, t.Any]) -> None:
     """
     Force desktop_mode to true in config file.
 
+    Rmove workspaces other than default workspace.  
+
     :param config: actual configuration
     """
     config["desktop_mode"] = True
+
+    if "storage" not in config:
+        logger.error("storage missing in config file ", config)
+        return
+    if "workspaces" not in config["storage"]:
+        logger.error("storage missing in config file ", config)
+        return
+    
+    workspaces = config["storage"]["workspaces"]
+
+    keys_to_remove = [key for key in workspaces if key != "default"]
+    for key in keys_to_remove:
+        del workspaces[key]
